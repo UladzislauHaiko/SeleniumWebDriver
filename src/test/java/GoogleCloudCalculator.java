f63f774aa70741e4eb712c4f2d4a52b46fac6d1d
UladzislauHaiko <tranceperfect@gmail.com>
2021-06-30 22:50:02 +0300

    feat: create auto-tests for selenium web driver home work

--- src/test/java/GoogleCloudCalculator.java
+++ src/test/java/GoogleCloudCalculator.java
@@ -9,6 +9,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -19,7 +21,7 @@ public class GoogleCloudCalculator {
 
     public void browserSetup(){
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
+        options.addArguments("incognito", "start-maximized");
         driver = new ChromeDriver(options);
 
     }
@@ -35,93 +37,109 @@ public class GoogleCloudCalculator {
         WebElement searchInput = driver.findElement(By.xpath("//input[contains(@class, 'devsite-search-field')]"));
         searchInput.sendKeys("Google Cloud Platform Pricing Calculator");
         searchInput.sendKeys(Keys.ENTER);
-        Thread.sleep(5000);
+
+        new WebDriverWait(driver,10)
+                .until(ExpectedConditions.presenceOfElementLocated
+                        (By.xpath("//div[contains(@class, 'gs-title')]//*[normalize-space() = 'Google Cloud Platform Pricing Calculator']")));
 
         String linkToOpen= driver.findElement(By.xpath("//div[contains(@class, 'gs-title')]//*[normalize-space() = 'Google Cloud Platform Pricing Calculator']")).getAttribute("href");
         driver.get(linkToOpen);
-        Thread.sleep(5000);
+
+        new WebDriverWait(driver,10)
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("//iFrame[contains(@src, '/products/calculator/index_')]")));
 
         WebElement frame = driver.findElement(By.xpath("//iFrame[contains(@src, '/products/calculator/index_')]"));
         driver.switchTo().frame(frame);
         driver.switchTo().frame("myFrame");
 
+        new WebDriverWait(driver,10)
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("(//*[@class='hexagon'and @title='Compute Engine']/*[@class='hexagon-in1']/*[@class='hexagon-in2'])[1]")));
         WebElement computeEngine = driver.findElement(By.xpath("(//*[@class='hexagon'and @title='Compute Engine']/*[@class='hexagon-in1']/*[@class='hexagon-in2'])[1]"));
-        Thread.sleep(5000);
         computeEngine.click();
-
 
         WebElement numberOfInstances = driver.findElement(By.xpath("//input[@id='input_65' and @name='quantity']"));
         numberOfInstances.sendKeys("4");
 
         WebElement operatingSystemDropdown = driver.findElement(By.xpath("//*[@ng-model='listingCtrl.computeServer.os']"));
         operatingSystemDropdown.click();
+        new WebDriverWait(driver,10)
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("//*[@class='md-ink-ripple']//*[normalize-space() = 'Free: Debian, CentOS, CoreOS, Ubuntu, or other User Provided OS']")));
         WebElement operatingSystemValue = driver.findElement(By.xpath("//*[@class='md-ink-ripple']//*[normalize-space() = 'Free: Debian, CentOS, CoreOS, Ubuntu, or other User Provided OS']"));
         operatingSystemValue.click();
 
         WebElement vmClass = driver.findElement(By.xpath("//*[@ng-model='listingCtrl.computeServer.class']"));
-        Thread.sleep(5000);
         vmClass.click();
+        new WebDriverWait(driver,10)
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("(//*[@class='md-ink-ripple']//*[normalize-space() = 'Regular'])[2]")));
         WebElement vmClassvalue = driver.findElement(By.xpath(("(//*[@class='md-ink-ripple']//*[normalize-space() = 'Regular'])[2]")));
-        Thread.sleep(3000);
         vmClassvalue.click();
 
         WebElement seriesDpn = driver.findElement(By.xpath("//*[@ng-model='listingCtrl.computeServer.series']"));
-        Thread.sleep(3000);
         seriesDpn.click();
+        new WebDriverWait(driver,10)
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("//*[@class= 'ng-scope md-ink-ripple']//*[normalize-space() = 'N1']")));
         WebElement seriesValue = driver.findElement(By.xpath("//*[@class= 'ng-scope md-ink-ripple']//*[normalize-space() = 'N1']"));
-        Thread.sleep(3000);
         seriesValue.click();
 
         WebElement machineTypeDpn = driver.findElement(By.xpath("//*[@ng-model='listingCtrl.computeServer.instance']"));
-        Thread.sleep(3000);
         machineTypeDpn.click();
+        new WebDriverWait(driver,10)
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("//*[@class= 'ng-scope md-ink-ripple']//*[normalize-space() = 'n1-standard-8 (vCPUs: 8, RAM: 30GB)']")));
         WebElement machineValue = driver.findElement(By.xpath("//*[@class= 'ng-scope md-ink-ripple']//*[normalize-space() = 'n1-standard-8 (vCPUs: 8, RAM: 30GB)']"));
-        Thread.sleep(3000);
         machineValue.click();
 
-        WebElement addGPUDpn = driver.findElement(By.xpath("//*[@ng-model='listingCtrl.computeServer.addGPUs']"));
-        Thread.sleep(3000);
-        addGPUDpn.click();
+        WebElement addGPUBtn = driver.findElement(By.xpath("//*[@ng-model='listingCtrl.computeServer.addGPUs']"));
+        addGPUBtn.click();
 
         WebElement nmbGPUDpn = driver.findElement(By.xpath("//*[@ng-model='listingCtrl.computeServer.gpuCount']"));
-        Thread.sleep(3000);
         nmbGPUDpn.click();
+        new WebDriverWait(driver,10)
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("//*[@class= 'ng-scope md-ink-ripple']//*[normalize-space() = '1']")));
         WebElement nmbGPUValue = driver.findElement(By.xpath("//*[@class= 'ng-scope md-ink-ripple']//*[normalize-space() = '1']"));
-        Thread.sleep(3000);
         nmbGPUValue.click();
 
         WebElement typeGPUDpn = driver.findElement(By.xpath("//*[@ng-model='listingCtrl.computeServer.gpuType']"));
-        Thread.sleep(3000);
         typeGPUDpn.click();
+        new WebDriverWait(driver,10)
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("//*[@class= 'ng-scope md-ink-ripple']//*[normalize-space() = 'NVIDIA Tesla V100']")));
         WebElement typeGPUValue = driver.findElement(By.xpath("//*[@class= 'ng-scope md-ink-ripple']//*[normalize-space() = 'NVIDIA Tesla V100']"));
-        Thread.sleep(3000);
         typeGPUValue.click();
 
         WebElement localSSDDpn = driver.findElement(By.xpath("//*[@ng-model='listingCtrl.computeServer.ssd']"));
-        Thread.sleep(3000);
         localSSDDpn.click();
+        new WebDriverWait(driver,10)
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("//*[@class= 'ng-scope md-ink-ripple']//*[normalize-space() = '2x375 GB']")));
         WebElement localSSDValue = driver.findElement(By.xpath("//*[@class= 'ng-scope md-ink-ripple']//*[normalize-space() = '2x375 GB']"));
-        Thread.sleep(3000);
         localSSDValue.click();
 
         WebElement dataLocDpn = driver.findElement(By.xpath("//*[@ng-model='listingCtrl.computeServer.location']"));
-        Thread.sleep(3000);
         dataLocDpn.click();
+        new WebDriverWait(driver,10)
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("(//*[@class='ng-scope md-ink-ripple']//*[normalize-space() = 'Frankfurt (europe-west3)'])[3]")));
         WebElement dataLocValue = driver.findElement(By.xpath(("(//*[@class='ng-scope md-ink-ripple']//*[normalize-space() = 'Frankfurt (europe-west3)'])[3]")));
-        Thread.sleep(3000);
         dataLocValue.click();
 
         WebElement usageDpn = driver.findElement(By.xpath("//*[@ng-model='listingCtrl.computeServer.cud']"));
-        Thread.sleep(3000);
         usageDpn.click();
+        new WebDriverWait(driver,10)
+                .until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("(//*[@class= 'md-ink-ripple']//*[normalize-space() = '1 Year'])[2]")));
         WebElement usageValue = driver.findElement(By.xpath("(//*[@class= 'md-ink-ripple']//*[normalize-space() = '1 Year'])[2]"));
-        Thread.sleep(3000);
         usageValue.click();
 
         WebElement estimateBtn = driver.findElement(By.xpath("(//*[@class= 'md-raised md-primary cpc-button md-button md-ink-ripple'])[1]"));
-        Thread.sleep(3000);
         estimateBtn.click();
-        Thread.sleep(3000);
+
 
         WebElement vmClassResult = driver.findElement(By.xpath("(//*[@class= 'md-list-item-text ng-binding'])[2]"));
         String vmResult = vmClassResult.getText();
@@ -153,8 +171,6 @@ public class GoogleCloudCalculator {
         System.out.println(costResult);
         Assert.assertEquals(costResult, "Total Estimated Cost: USD 1,082.77 per 1 month");
 
-
-        Thread.sleep(10000);
 
         driver.quit();
 
